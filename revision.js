ee567dfa9cb9c21ffb9ff058e4e1b0e87c8f10e9
sunilnikam31 <sunilnikam31@gmail.com>
2018-08-06 13:59:08 +0530

    first test passing

--- revision.js
+++ revision.js
@@ -1,5 +1,5 @@
 var r = {};
-
+//fibonacci
 r.fibonacci = function (position) {
     if (position < 1) return;
     if (Math.floor(position) != position) return;
@@ -10,6 +10,20 @@ r.fibonacci = function (position) {
         return 1;
     }
     return r.fibonacci(position-1) + r.fibonacci(position -2);
+}
+//reverse text
+r.reverseText = function (str){
+    splitString = str.split("");
+    reverseArray = splitString.reverse();
+    joinArray = reverseArray.join("");
+    return joinArray;
+}
+//reverse text with white spaces
+r.reverseText = function (str){
+    splitString = str.split("");
+    reverseArray = splitString.reverse();
+    joinArray = reverseArray.join("");
+    return joinArray;
 }
 
 exports.r = r;
