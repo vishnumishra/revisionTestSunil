385ebad8f71d7d5166797f624697072bf2770451
sunilnikam31 <sunilnikam31@gmail.com>
2018-08-06 13:26:23 +0530

    first test passing

--- revision.js
+++ revision.js
@@ -1,7 +1,15 @@
 var r = {};
 
 r.fibonacci = function (position) {
-
+    if (position < 1) return;
+    if (Math.floor(position) != position) return;
+    if (position == 1) {
+        return 0;
+    }
+    if(position == 2){
+        return 1;
+    }
+    return r.fibonacci(position-1) + r.fibonacci(position -2);
 }
 
 exports.r = r;
